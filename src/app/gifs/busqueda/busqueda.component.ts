42cbbd2592b09355fd870a5893fbea65cd8b3040
Erika <erikapalmasan@gmail.com>
2021-09-16 19:45:34 +0200

    capturar valor de input con @viewChild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.scss']
+  styleUrls: ['./busqueda.component.scss'],
 })
 export class BusquedaComponent implements OnInit {
+  @ViewChild('txtBuscar') txtBuscar: ElementRef<HTMLInputElement>;
+  buscar(): void {
+    const valor = this.txtBuscar.nativeElement.value;
 
-  constructor() { }
+    console.log(valor);
 
-  ngOnInit(): void {
+    this.txtBuscar.nativeElement.value = '';
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
