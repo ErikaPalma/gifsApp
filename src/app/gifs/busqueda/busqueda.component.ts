fadd3f9b959050717c2291b4a868d724cd90f7d5
Erika <erikapalmasan@gmail.com>
2022-08-29 13:03:19 +0200

    Referencias a objetos del HTML con @viewChild - Creación del servicio

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.scss']
+  styleUrls: ['./busqueda.component.scss'],
 })
 export class BusquedaComponent implements OnInit {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
